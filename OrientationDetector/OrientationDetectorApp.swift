5f449d990e9b7427f983a83c8ea235c36cd47e6f
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-07-17 09:51:02 +0900

    Initial Commit

---
+++ OrientationDetector/OrientationDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationDetectorApp.swift
+//  OrientationDetector
+//
+//  Created by MacBook Pro on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct OrientationDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
